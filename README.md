19de6682f72ff05bcccd1529c244054a4554c50b
Chris Pick <chris@chrispick.com>
2023-04-19 16:53:51 -0400

    Create a local-only, beefier nixpkgs#darwin.builder
    `nix run` will start a builder VM bound to localhost using 8 CPUs and
    listening on port 2222.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Darwin builder
+
+Use `nix run` to start a
+[nixpkgs#darwin.builder](https://nixos.org/manual/nixpkgs/stable/#sec-darwin-builder)
+that's bound to localhost, uses 8 CPUs, and listens on port 2222.
--- flake.nix
+++ flake.nix
@@ -1,11 +1,37 @@
 {
-  description = "A very basic flake";
+  description = "Local-only, beefier Darwin builder";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:cpick/nixpkgs/macos-builder-options";
   };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-darwin";
+      pkgs = nixpkgs.legacyPackages."${system}";
+      linuxSystem = builtins.replaceStrings [ "darwin" ] [ "linux" ] system;
+
+      nixos = import "${nixpkgs}/nixos" {
+        configuration = {
+          imports = [
+            "${nixpkgs}/nixos/modules/profiles/macos-builder.nix"
+          ];
+
+          virtualisation.host = { inherit pkgs; };
+          virtualisation.darwin-builder.cores = nixpkgs.lib.mkForce 8;
+          virtualisation.darwin-builder.hostAddress = nixpkgs.lib.mkForce "127.0.0.1";
+          virtualisation.darwin-builder.hostPort = nixpkgs.lib.mkForce 2222;
+        };
+
+        system = linuxSystem;
+      };
+
+    in {
+
+      packages.x86_64-darwin.builder =
+        nixos.config.system.build.macos-builder-installer;
+
+      packages.x86_64-darwin.default = self.packages.x86_64-darwin.builder;
+
+    };
 }
