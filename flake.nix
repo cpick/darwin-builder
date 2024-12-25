a908bbdb98dbf27fec231dcd5c59ae8732afe509
Chris Pick <chris@chrispick.com>
2024-12-25 13:15:35 -0800

    Format flake.nix with `nixfmt`

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   description = "Local-only, beefier Darwin builder";
 
-  inputs = {
-    nixpkgs.url = "github:cpick/nixpkgs/nix-builder-vm-options";
-  };
+  inputs = { nixpkgs.url = "github:cpick/nixpkgs/nix-builder-vm-options"; };
 
   outputs = { self, nixpkgs }:
     let
@@ -13,13 +11,12 @@
 
       nixos = import "${nixpkgs}/nixos" {
         configuration = {
-          imports = [
-            "${nixpkgs}/nixos/modules/profiles/nix-builder-vm.nix"
-          ];
+          imports = [ "${nixpkgs}/nixos/modules/profiles/nix-builder-vm.nix" ];
 
           virtualisation.host = { inherit pkgs; };
           virtualisation.darwin-builder.cores = nixpkgs.lib.mkForce 8;
-          virtualisation.darwin-builder.hostAddress = nixpkgs.lib.mkForce "127.0.0.1";
+          virtualisation.darwin-builder.hostAddress =
+            nixpkgs.lib.mkForce "127.0.0.1";
           virtualisation.darwin-builder.hostPort = nixpkgs.lib.mkForce 2222;
         };
 
@@ -27,9 +24,9 @@
       };
 
     in {
-      packages.${system} = {
+      packages."${system}" = {
         builder = nixos.config.system.build.macos-builder-installer;
-        default = self.packages.${system}.builder;
+        default = self.packages."${system}".builder;
       };
     };
 }
